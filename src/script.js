14a4a87c8ff51c4d91e66b7e95cc63d88d3ef13a
anuragv668 <anuragvarma668@gmail.com>
2024-10-25 16:25:28 +0530

    make api request and set the quote

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log("hello");
+const url = 'https://qapi.vercel.app/api/random';
+const text = document.getElementById('quote');
+const setter = (quote) => {
+  text.textContent = quote;
+}
+
+const quoteGetter = () => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setter(data.quote)
+    })
+}
+
+setter(quoteGetter(url));
